68fa647bfddc57199f9a240975bbf7a853d19441
Qiu Juntao <juntao.qiu@gmail.com>
2015-10-24 19:46:26 +1100

    make the code looks a little bit better

--- extract_bookmarks.py
+++ extract_bookmarks.py
@@ -1,42 +1,41 @@
 # encoding=utf-8
-import nltk
 import jieba
 
 import matplotlib
 import matplotlib.pyplot as plt
 
 from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
+from pandas import DataFrame
+from collections import Counter
 
-soup = BeautifulSoup(open('bookmarks_10_21_15.html').read(), "html.parser")
-data = soup.get_text()
+def load_bookmarks_data():
+	soup = BeautifulSoup(open('bookmarks_10_21_15.html').read(), "html.parser")
+	return soup.get_text()
 
-stoplist = stopwords.words('english')
-stoplist.extend(['...', 'com', 'using', u'使用', 'blog', u'博客', u'博客园', u'做法', u'论坛', u'部分', u'天下'])
+def extract_segments(data):
+	seg_list = jieba.cut(data, cut_all=False)
+	return [seg.strip().lower() for seg in seg_list if len(seg) > 1]
 
-seg_list = jieba.cut(data, cut_all=False)
-filtered = [seg.strip().lower() for seg in seg_list if len(seg) > 1]
-freq_dist_nltk = nltk.FreqDist([word for word in filtered if word not in stoplist])	
+def tokenize():	
+	stoplist = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+	stoplist.extend(['...', 'com', 'using', u'使用', 'blog', u'博客', u'博客园', u'做法', u'论坛', 'part', u'部分', u'天下'])
+	filtered = extract_segments(load_bookmarks_data())
+	
+	return Counter([word for word in filtered if word not in stoplist])
 
-# import json
-# print json.dumps(dict(freq_dist_nltk.most_common(100)), ensure_ascii=False).encode('utf8')
+def visualize():
+	frame = DataFrame(tokenize().most_common(20), columns=['keywords', 'frequencies'])
+	myfont = matplotlib.font_manager.FontProperties(fname='/Library/Fonts/AdobeSongStd-Light.otf') 
 
-# for item in freq_dist_nltk.most_common(100):
-#     print unicode(item[0])
+	plt.style.use('ggplot')
+	
+	ax = frame.plot(kind='bar')
+	for rect, keyw in zip(ax.patches, frame['keywords']) :
+	    height = rect.get_height()
+	    ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, unicode(keyw),
+	                 ha='center', va='bottom', fontproperties=myfont, rotation=45)
 
-import pandas as pd
-from pandas import DataFrame, Series
+	plt.savefig('bookmarks_trending.png', format='png', dpi=600, fontproperties=myfont)   
 
-frame = DataFrame(freq_dist_nltk.most_common(20), columns=['keywords', 'frequencies'])
-
-myfont = matplotlib.font_manager.FontProperties(fname='/Library/Fonts/AdobeSongStd-Light.otf') 
-
-ax = frame.plot(kind='bar')
-for rect, keyw in zip(ax.patches, frame['keywords']) :
-    height = rect.get_height()
-    ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, unicode(keyw),
-                 ha='center', va='bottom', fontproperties=myfont, rotation=45)
-
-# legend(bbox_to_anchor=(1.02, 1), loc=2, borderaxespad=0.)
-plt.savefig('destination_path.png', format='png', dpi=600, fontproperties=myfont)   
-
+if __name__ == "__main__":
+	visualize()
